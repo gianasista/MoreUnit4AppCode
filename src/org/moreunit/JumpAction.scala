ee70b7d6798e6f6933b44f7515b702b68da67305
gianasista <gianasista@users.sourceforge.net>
2012-10-15 12:55:47 +0200

    Action class in Scala now

---
+++ src/org/moreunit/JumpAction.scala
@@ -0,0 +1,30 @@
+package org.moreunit
+
+import com.intellij.openapi.actionSystem.{AnActionEvent, AnAction}
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.psi.{PsiDocumentManager, PsiFile}
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vera
+ * Date: 15.10.12
+ * Time: 12:41
+ * To change this template use File | Settings | File Templates.
+ */
+class JumpAction extends AnAction {
+
+  def actionPerformed(event: AnActionEvent): Unit =
+  {
+    val project = event.getProject
+    val editor = FileEditorManager.getInstance(project).getSelectedTextEditor
+    val file = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument)
+
+    val jumper = new JumpLogic(file)
+    val target = jumper.target
+
+    if (target != null)
+      FileEditorManager.getInstance(project).openFile(target, true)
+  }
+}
